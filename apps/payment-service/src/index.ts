6097a20961643dcf09402469868ea8e45d9aebcd
Tirth02 <pateltirth0902@gmail.com>
2026-08-17 21:36:15 -0400

    payment service created and tested

---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,28 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+// import process from "process";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const start = async () => {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 8002,
+      },
+      (info) => {
+        console.log(`Payment service is running on port 8002`);
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    // process.exit(1);
+  }
+};
+
+start();
