e68a0e5463e60865ed435ec8271be354eca66fbb
Tirth02 <pateltirth0902@gmail.com>
2026-08-19 17:42:15 -0400

    middleware implementation in payment(hono)

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { shouldBeUser } from "./middleware/auth.middleware.js";
 // import process from "process";
 
 const app = new Hono();
@@ -10,18 +11,12 @@ app.get("/", (c) => {
   return c.text("Payment endpoint works!!");
 });
 
-app.get("/test", (c) => {
-    const auth = getAuth(c);
+app.get("/test",shouldBeUser, (c) => {
     
-    if(!auth.userId)
-    {
-      return c.json({
-      message: 'You are not logged in.',
-      })
-    }
 
     return c.json({
-    message: 'Payment Service Authenticated'
+    message: 'Payment Service Authenticated',
+    userId: c.get("userId")
   })
 })
 
