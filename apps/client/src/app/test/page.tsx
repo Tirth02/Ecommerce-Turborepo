4d26c04b73bde7ceda515fcd129b3d5333534b29
Tirth02 <pateltirth0902@gmail.com>
2026-08-19 13:39:16 -0400

    implemented authentication in orderservice

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -3,18 +3,33 @@ import { auth } from "@clerk/nextjs/server";
 const TestPage = async () => {
     const {getToken} = await auth();
     const token = await getToken();
-    const res = await fetch("http://localhost:8000/test",{
+    
+    const orderRes = await fetch("http://localhost:8001/test",{
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
-    const data =  await res.json();
+
+    const orderData = await orderRes.json();
+
+
+    const resProduct = await fetch("http://localhost:8000/test",{
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    const dataProduct =  await resProduct.json();
 
     
-    console.log(data);
+    // console.log(dataProduct);
 
   return (
-    <div>{data.message}</div>
+    <div>
+      <h1>Product Authentication status : {dataProduct.message}</h1>
+      <h1>Order Authentication status : {orderData.message}</h1>
+      
+    </div>
+
   )
 }
 
--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,9 +1,27 @@
 import Fastify from "fastify";
+import { clerkPlugin, getAuth } from '@clerk/fastify'
 
 const fastify = Fastify();
 
+fastify.register(clerkPlugin)
+
 fastify.get("/",(request,reply) => {
     return reply.send("Order endpoint works");
+})
+
+fastify.get("/test",(request,reply) => {
+    const {userId} = getAuth(request);
+
+    if(!userId)
+    {
+        return reply.send({message: "You are not Authenticated yet!!!"});
+    }
+    else
+    {
+        return reply.send({message: "Order Service Authenticated!!!"});
+ 
+    }
+    
 })
 const start = async() => {
     try{
